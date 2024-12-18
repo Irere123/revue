188a0f55c8cc852d875d1f406c973211481f8c7f
Irere123 <irere2020@outlook.com>
2024-12-18 09:21:18 +0200

    setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,19 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import "dotenv/config";
+import * as express from "express";
 
-AppDataSource.initialize().then(async () => {
+const PORT = process.env.PORT || 4000;
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+async function main() {
+  const app = express();
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+  app.get("/", (_req, res) => {
+    res.send("Hello world");
+  });
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+  app.listen(PORT, () => {
+    console.log(`🚀 Server started running on ${PORT}`);
+  });
+}
 
-}).catch(error => console.log(error))
+main();
