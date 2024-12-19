713bac532766bb4d727041e9ae1845b0abb52e6c
Irere123 <irere2020@outlook.com>
2024-12-19 17:25:20 +0200

    fix: remove ssl

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -10,7 +10,6 @@ export const AppDataSource = new DataSource({
     process.env.DATABASE_URL ||
     "potsgres://postgres:postgres@localhost:5432/revue",
   entities: [User],
-  ssl: IS_PROD,
   logging: !IS_PROD,
   migrations: ["build/migration/*.js"],
 });
